5e1022b5546503280fa64988d38bc40e8212ef2d
Manuu2211 <mansikiloskar1432@gmail.com>
2023-03-25 15:46:07 +0530

    Updates

---
+++ precache-manifest.b9e0dcf70f2e0eafd3399e3f55674772.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "45ee322a66459bd52278",
+    "url": "/Calculator/static/css/main.a51036f0.chunk.css"
+  },
+  {
+    "revision": "45ee322a66459bd52278",
+    "url": "/Calculator/static/js/main.45ee322a.chunk.js"
+  },
+  {
+    "revision": "927bd4bacd441db6510d",
+    "url": "/Calculator/static/js/1.927bd4ba.chunk.js"
+  },
+  {
+    "revision": "20019c294ea6a7cce4c0",
+    "url": "/Calculator/static/js/2.20019c29.chunk.js"
+  },
+  {
+    "revision": "4d82e308a4abb79010f8",
+    "url": "/Calculator/static/js/runtime~main.4d82e308.js"
+  },
+  {
+    "revision": "1784d6df71fa14c1f62f2393d05d5476",
+    "url": "/Calculator/index.html"
+  }
+];
